942b2f4c68be818b810715602a83c0c34f04a319
jokertitcho <jokertitcho@yahoo.fr>
2024-04-10 13:55:21 +0200

    blabla

---
+++ src/componants/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <p>Contactez moi sur bjkalalr@gmal.con </p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/componants/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <h1>Bienvenue sur la page d'accueil</h1>
+            </header>
+        </div>
+    );
+};
+
+export default HomePage;
